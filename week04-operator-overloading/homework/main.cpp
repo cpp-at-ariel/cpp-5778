e3d4460ed071f52f379552544d31e3a358213211
Erel Segal-Halevi cron <erelsgl@gmail.com>
2018-04-12 10:26:10 +0300

    doc

--- week04-operator-overloading/homework/main.cpp
+++ week04-operator-overloading/homework/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 
 int main() {
-	CircularInt hour {1, 12};                 // Define an integer between 1 and 12.
+	CircularInt hour {1, 12};                 // <hour is an integer between 1 and 12, like an hour on the clock>
 	cout << hour << endl;                     // 1
 	hour += 4;  cout << hour << endl;         // 5
 	(hour += 2)++;  cout << hour << endl;     // 8
