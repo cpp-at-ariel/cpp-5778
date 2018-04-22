ef7efbe3ce16dacbb88a376610faa6b0cf903c83
Erel Segal-Halevi cron <erelsgl@gmail.com>
2018-04-22 20:10:57 +0300

    move homework

---
+++ week05-copy-friend/homework/main.cpp
@@ -0,0 +1,55 @@
+#include "Board.h"
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	Board board1{4};  // Initializes a 4x4 board
+	cout << board1 << endl;   /* Shows an empty board:
+	....
+	....
+	....
+	....
+	*/
+	board1[{1,1}]='X';
+	board1[{1,2}]='O';
+	cout << board1 << endl;  /* Shows the following board:
+	....
+	.XO.
+	....
+	....
+	*/
+
+	try {
+		board1[{3,4}]='O';   // This should raise an exception
+	} catch (IllegalCoordinateException ex) {
+		cout << "Illegal coordinate: " << ex.theCoordinate() << endl;  // prints "Illegal coordinate: 3,4"
+	}
+
+	board1 = '.';     // Fill the entire board with "."
+	cout << board1 << endl;  /* Shows an empty board, as above */
+	try { 
+		board1 = 'a';        // This should raise an exception
+	} catch (IllegalCharException ex) {
+		cout << "Illegal char: " << ex.theChar() << endl;  // "Illegal char: a"
+	}
+
+	try {
+		board1[{0,1}] = 'x';  // This should raise an exception
+	} catch (IllegalCharException ex) {
+		cout << "Illegal char: " << ex.theChar() << endl;  // "Illegal char: x"
+	}
+
+	Board board2 = board1;
+	board2[{0,0}] = 'X';
+	cout << board1 << endl;  /* Shows an empty board, as above */
+	cout << board2 << endl;  /* Shows a board with an X at top-left */
+
+	board1 = board2;
+	board1[{3,3}] = 'O';
+	cout << board2 << endl;  /* Shows a board with an X at top-left */
+	cout << board1 << endl;  /* Shows a board with an X at top-left and O at bottom-right */
+
+	return 0;
+}
+
--- week06-inheritance/homework/main.cpp
+++ week06-inheritance/homework/main.cpp
@@ -1,55 +1,23 @@
-#include "Board.h"
 
-#include <iostream>
-using namespace std;
+#include "TicTacToe.h"
+#include "Player.h"
+#include "Board.h"
+#include "Move.h"
+#include "Winner.h"
+
+class Loser: public Player {
+	public:
+		Move play(Board& board) {
+			return Move();
+		}
+};
 
 int main() {
-	Board board1{4};  // Initializes a 4x4 board
-	cout << board1 << endl;   /* Shows an empty board:
-	....
-	....
-	....
-	....
-	*/
-	board1[{1,1}]='X';
-	board1[{1,2}]='O';
-	cout << board1 << endl;  /* Shows the following board:
-	....
-	.XO.
-	....
-	....
-	*/
-
-	try {
-		board1[{3,4}]='O';   // This should raise an exception
-	} catch (IllegalCoordinateException ex) {
-		cout << "Illegal coordinate: " << ex.theCoordinate() << endl;  // prints "Illegal coordinate: 3,4"
-	}
-
-	board1 = '.';     // Fill the entire board with "."
-	cout << board1 << endl;  /* Shows an empty board, as above */
-	try { 
-		board1 = 'a';        // This should raise an exception
-	} catch (IllegalCharException ex) {
-		cout << "Illegal char: " << ex.theChar() << endl;  // "Illegal char: a"
-	}
-
-	try {
-		board1[{0,1}] = 'x';  // This should raise an exception
-	} catch (IllegalCharException ex) {
-		cout << "Illegal char: " << ex.theChar() << endl;  // "Illegal char: x"
-	}
-
-	Board board2 = board1;
-	board2[{0,0}] = 'X';
-	cout << board1 << endl;  /* Shows an empty board, as above */
-	cout << board2 << endl;  /* Shows a board with an X at top-left */
-
-	board1 = board2;
-	board1[{3,3}] = 'O';
-	cout << board2 << endl;  /* Shows a board with an X at top-left */
-	cout << board1 << endl;  /* Shows a board with an X at top-left and O at bottom-right */
-
+	TicTacToe game(4);       // Initializes a game on a 4x4 board
+	Loser loser;             // Creates a player with a lousy strategy
+	Winner winner;           // Creates YOUR player, with a winning strategy
+	cout << game.run(loser, winner); // returns 0 if the first player wins, 1 if the seccond wins. Hopefully it will print 1 since your player will win.
+	cout << game.run(winner, loser); // returns 0 if the first player wins, 1 if the seccond wins. Hopefully it will print 0 since your player will win.
 	return 0;
 }
 
