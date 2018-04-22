0459008453ab69579290403fa0eed2911faf7e5b
Erel Segal-Halevi cron <erelsgl@gmail.com>
2018-04-22 14:31:22 +0300

    week 4

--- week04-operator-overloading/4-parentheses-operator/main.cpp
+++ week04-operator-overloading/4-parentheses-operator/main.cpp
@@ -27,33 +27,18 @@ void repeat(int n, MyFunctor action) {
 void filter(vector<int> v, MyFunctor condition) {
 	for (int i=0; i<v.size(); ++i)
 		if (condition(v[i]))
-		// if (condition.operator()(v[i]))
+		// equivalent to: if (condition.operator()(v[i]))
 			cout << v[i] << " ";
 }
 
-void g() {
-	cout << "#";
-}
-
 int main() {
-	MyFunctor f{};
-	// NOT equivalent to: MyFunctor f();
-	//
-	// MyFunctor f{'*'};
-	// equivalent to: MyFunctor f('*');  
+	MyFunctor f;
 	f(); cout << endl; // prints a star
-	// equivalent to:
 	cout << endl << f(5) << " " << f(-5) << endl;
 
 	repeat(10, f); cout << endl; // print 10 stars
 	repeat(10, MyFunctor('^'));    cout << endl;
+
 	vector<int> v {-5,4,-3,2,-1,0};
 	filter(v, f);  // prints 4, 2
-
-	cout << "---" << endl;
-	//MyFunctor h(7);  // calls constructor
-	MyFunctor h{7};  // calls constructor
-	cout << "---" << endl;
-	h(7);  // calls operator
-	// h{7};  // won't compile
 }
